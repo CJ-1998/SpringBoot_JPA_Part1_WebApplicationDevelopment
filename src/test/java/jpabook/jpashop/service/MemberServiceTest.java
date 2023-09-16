0ca24772142416f9da650512242f569b60c990e3
CJ <choijun0627@konkuk.ac.kr>
2023-09-16 20:35:55 +0900

    <feat> 스프링부트와 JPA 활용 1편 프로젝트 section 4
    회원 기능 테스트
    
    회원 기능에 회원 가입과 중복 회원 예외 발생 잘 되는지 테스트하기 위해
    MemberServiceTest 생성
    그리고 테스트에서 설정을 운영과 다르게 하기 위해 그리고 메모리 모드로
    메모리 DB를 사용하기 위해 application.yml을 test-resources에 생성

---
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,52 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+//@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    public void 회원가입() throws Exception {
+        //Given
+        Member member = new Member();
+        member.setName("kim");
+        //When
+        Long saveId = memberService.join(member);
+        //Then
+        assertEquals(member, memberRepository.findOne(saveId));
+    }
+
+//    @Test(expected = IllegalStateException.class)
+    @Test
+    public void 중복_회원_예외() throws Exception {
+        //Given
+        Member member1 = new Member();
+        member1.setName("kim");
+        Member member2 = new Member();
+        member2.setName("kim");
+
+        //When
+        memberService.join(member1);
+        assertThatThrownBy(()->memberService.join(member2)).isInstanceOf(IllegalStateException.class); //예외가 발생해야 한다.
+
+        //Then
+        //fail("예외가 발생해야 한다.");
+    }
+}
