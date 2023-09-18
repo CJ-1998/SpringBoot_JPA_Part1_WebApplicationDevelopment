c6e37b02684f5d85423aca8e38d56ef29076badd
CJ <choijun0627@konkuk.ac.kr>
2023-09-18 22:02:02 +0900

    <feat> 스프링부트와 JPA 활용 1편 프로젝트 section 7
    상품 목록 화면
    
    ItemController에서 itemService.findItems() 해서 Item들 List 받아서
    Model에 담아서 뷰에 넘기는 것이다
    itmeList.html 생성해서 이전처럼 th:each로 루프 돌면서 뿌린다
    수정 버튼이 생겼다

--- src/main/java/jpabook/jpashop/controller/ItemController.java
+++ src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -32,4 +32,14 @@ public class ItemController {
         itemService.saveItem(book);
         return "redirect:/items";
     }
+
+    /**
+     * 상품 목록
+     */
+    @GetMapping(value = "/items")
+    public String list(Model model) {
+        List<Item> items = itemService.findItems();
+        model.addAttribute("items", items);
+        return "items/itemList";
+    }
 }
