4161a72b51f1593fde9aebc11a5bac2e9eb1b6e7
CJ <choijun0627@konkuk.ac.kr>
2023-09-19 02:00:25 +0900

    <feat> 스프링부트와 JPA 활용 1편 프로젝트 section 7
    변경 감지와 병합
    
    업데이트 할 때 변경 감지와 병합에 대해 배우면서 병합 사용하지 말고 변경 감지 사용해야 함을 배웠다
    변경 감지 사용하기 위해 ItemService에서 트랜잭션 있는 곳에서 Item을
    DB에서 찾고 값을 변경하는 updateItem 함수를 생성
    그리고 ItemController에서는 직접 엔티티 생성해서 setter로 하는 것이
    아니라 ItemService에 updateItem 함수 호출하게 변경

--- src/main/java/jpabook/jpashop/controller/ItemController.java
+++ src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -67,17 +67,20 @@ public class ItemController {
      * 상품 수정
      */
     @PostMapping(value = "/items/{itemId}/edit")
-    public String updateItem(@ModelAttribute("form") BookForm form) {
-        Book book = new Book();
+    public String updateItem(@PathVariable Long itemId, @ModelAttribute("form") BookForm form) {
+//        Book book = new Book();
+//
+//        book.setId(form.getId());
+//        book.setName(form.getName());
+//        book.setPrice(form.getPrice());
+//        book.setStockQuantity(form.getStockQuantity());
+//        book.setAuthor(form.getAuthor());
+//        book.setIsbn(form.getIsbn());
+//
+//        itemService.saveItem(book);
 
-        book.setId(form.getId());
-        book.setName(form.getName());
-        book.setPrice(form.getPrice());
-        book.setStockQuantity(form.getStockQuantity());
-        book.setAuthor(form.getAuthor());
-        book.setIsbn(form.getIsbn());
+        itemService.updateItem(itemId,form.getName(),form.getPrice(),form.getStockQuantity());
 
-        itemService.saveItem(book);
         return "redirect:/items";
     }
 }
