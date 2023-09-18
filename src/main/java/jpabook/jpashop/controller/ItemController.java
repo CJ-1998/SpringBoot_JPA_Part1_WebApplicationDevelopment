b46729589d52a0fc2d387d83615e61d3c8d9fe65
CJ <choijun0627@konkuk.ac.kr>
2023-09-18 22:29:46 +0900

    <feat> 스프링부트와 JPA 활용 1편 프로젝트 section 7
    상품 수정
    
    상품 수정하기 위해 BookForm을 만들어서 model에 담아서 화면으로 넘긴다
    넘어온 화면이 updateItemForm.html이다
    여기서 값을 수정하고 다시 BookForm에 담아서 post 방식으로 넘긴다
    ItemController에서 @PostMapping으로 받아서 다시 BookForm을 Book으로 바꾼
    후 itemService에 saveItem 불러서 저장한다

--- src/main/java/jpabook/jpashop/controller/ItemController.java
+++ src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -42,4 +42,42 @@ public class ItemController {
         model.addAttribute("items", items);
         return "items/itemList";
     }
+
+    /**
+     * 상품 수정 폼
+     */
+    @GetMapping(value = "/items/{itemId}/edit")
+    public String updateItemForm(@PathVariable("itemId") Long itemId, Model model) {
+        Book item = (Book) itemService.findOne(itemId);
+
+        BookForm form = new BookForm();
+
+        form.setId(item.getId());
+        form.setName(item.getName());
+        form.setPrice(item.getPrice());
+        form.setStockQuantity(item.getStockQuantity());
+        form.setAuthor(item.getAuthor());
+        form.setIsbn(item.getIsbn());
+
+        model.addAttribute("form", form);
+        return "items/updateItemForm";
+    }
+
+    /**
+     * 상품 수정
+     */
+    @PostMapping(value = "/items/{itemId}/edit")
+    public String updateItem(@ModelAttribute("form") BookForm form) {
+        Book book = new Book();
+
+        book.setId(form.getId());
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+
+        itemService.saveItem(book);
+        return "redirect:/items";
+    }
 }
